e0210aa0e6c44a016c4ef60c4131bbd6374cd412
Simona Šusterová <simsusterova@gmail.com>
2025-04-19 15:38:32 +0200

    Zarovka hotovo

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log('funguju!');
+
+//1. úkol
+// const bulbElm = document.querySelector (".bulb");
+
+// document.addEventListener ("keydown" , () =>{
+//     bulbElm.classList.add("bulb--on")
+// });
+
+//2.úkol
+// const bulbElm = document.querySelector('.bulb');
+// document.addEventListener('keydown', () => {
+//   if (bulbElm.classList.contains('bulb--on')) {
+//     bulbElm.classList.remove('bulb--on');
+//   } else {
+//     bulbElm.classList.add('bulb--on');
+//   }
+// });
